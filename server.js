ba262d0ad3cb0b422db1367ec20d79b57f9bf591
Matheus Fernandes <matheusfffcorrea@gmail.com>
2025-08-20 14:36:27 -0300

    PokeDex usando API e NodeJs

---
+++ server.js
@@ -0,0 +1,9 @@
+// importa as coisas
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
+
+// cria servidor
+const server = express()
+const porta = 3333
+
