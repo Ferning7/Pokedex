56aa96cbb863014135066dbd2cf4b33bd0843df3
Matheus Fernandes <matheusfffcorrea@gmail.com>
2025-08-20 15:17:51 -0300

    feat: realizando requisições pra API

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 // importa as coisas
-const express = require('express')
-const axios = require('axios')
-const path = require('path')
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+const { error } = require("console");
 
 // cria servidor
-const server = express()
-const porta = 3333
+const server = express();
+const porta = 3333;
 
-server.use(express.static(path.join(__dirname, 'public')))
+/*
+configurando o express para servir arquivos estáticos da pasta 'public',
+quando o cliente acessar o servidor, ele irá encontrar os arquivos da pasta public
+*/
+server.use(express.static(path.join(__dirname, "public")));
 
-server.listen(porta, ()=>{
-    console.log(`Perigoso ta na ativa na porta ${porta}`)
-})
+/*Criando rota /pokemons quando o servidor receber uma requisição do tipo get
+na rota pokemons, o código será executado
+*/
+
+server.get("/pokemons", async (req, res) => {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const pokemons = response.data.results;
+
+    res.json(pokemons);
+
+  } 
+  catch (error) {
+    res.status(500).json({error: 'Erro ao buscar dados da PokeAPI'});
+  }
+});
+
+server.listen(porta, () => {
+  console.log(`Perigoso ta na ativa na porta ${porta}`);
+});
